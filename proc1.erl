9220a0082996dea738a3e088b5bfc1d53c5c3d2d
Dr. Alan U. Kennington <31448590+drauk@users.noreply.github.com>
2018-02-13 01:51:33 +1100

    Added another example of a two-process function.

--- proc1.erl
+++ proc1.erl
@@ -5,18 +5,42 @@
 % For sleep/1: http://erlang.org/doc/man/timer.html#sleep-1
 % For is_integer/1: http://erlang.org/doc/man/erlang.html#is_integer-1
 % For andalso: http://erlang.org/doc/reference_manual/expressions.html#id82894
+% For spawn/3: http://erlang.org/doc/man/erlang.html#spawn-3
 
 -module(proc1).
 
--export([procstartA/0, procA/2]).
+-export([procstartA/0, procstartAx/0, procA/2, procA/3]).
 
-procA(_, 0) ->
+%==============================================================================
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% 2-parameter version.
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+procA(S, 0) ->
+    io:format("~p~n", [S]),
     done;
 procA(S, Ntimes) when is_integer(Ntimes) andalso Ntimes >= 1 ->
     io:format("~p~n", [S]),
     timer:sleep(1000),
     procA(S, Ntimes - 1).
 
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% 3-parameter version.
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+procA(S, 0, _) ->
+    io:format("~p ~p~n", [S, 0]),
+    done;
+procA(S, Ntimes, Tsleep) when is_integer(Ntimes) andalso Ntimes >= 1
+        andalso is_number(Tsleep) andalso Tsleep >= 0 ->
+    io:format("~p ~p~n", [S, Ntimes]),
+    timer:sleep(Tsleep),
+    procA(S, Ntimes - 1, Tsleep).
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% Erlang automatically works out the arity of procA !!
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 procstartA() ->
     spawn(proc1, procA, ["process 1", 4]),
     spawn(proc1, procA, ["process 2", 4]).
+procstartAx() ->
+    spawn(proc1, procA, ["process 1000", 10, 1000]),
+    spawn(proc1, procA, ["process 2500", 10, 2500]).
