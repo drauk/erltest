1d53464a6d07c46bfbf2deff845e63b023530aa3
Dr. Alan U. Kennington <31448590+drauk@users.noreply.github.com>
2018-02-17 16:32:04 +1100

    Reorganised wxErlang event handling into nested case statements.

--- wx1.erl
+++ wx1.erl
@@ -1,4 +1,4 @@
-% src/erlang/wx1.erl   2018-2-16   Alan U. Kennington.
+% src/erlang/wx1.erl   2018-2-17   Alan U. Kennington.
 % $Id$
 % Familiarization with wxErlang library.
 % Reference manual: http://erlang.org/doc/apps/wx/index.html
@@ -41,26 +41,66 @@ createWindowA(ServerA) ->
     % Carriage return. Get the shell text cursor back to the left of the line.
     io:format("~n", []),
 
-    % I don't know what this does.
+    % Create tool bar.
+    % This doesn't create a tool bar, and doesn't cause a Glib/SCIM warning.
+    io:format("Calling wxFrame:createToolBar/2.~n", []),
+    wxFrame:createToolBar(FrameA, []),
+    % Carriage return. Get the shell text cursor back to the left of the line.
+%    io:format("~n", []),
+
+    % Get keyboard focus.
+    % This seems to have no effect.
+%    wxFrame:setFocus(FrameA),
+    wxWindow:setFocus(FrameA),
+
     % See http://erlang.org/doc/man/wxFrame.html
     % which says that wxFrame is derived from:
     % wxTopLevelWindow, wxWindow, wxEvtHandler
     % http://erlang.org/doc/man/wxEvtHandler.html#connect-2
     % wxCloseEventType() = close_window | end_session | query_end_session
     % Events defined in file: /usr/local/lib/erlang/lib/wx-1.8.3/include/wx.hrl
+    % wxClose
     wxFrame:connect(FrameA, close_window),
+    wxFrame:connect(FrameA, end_session),
+    wxFrame:connect(FrameA, query_end_session),
 
-    % Receive key events.
+    % Other event classes and types.
+    % wxChildFocus
+    wxFrame:connect(FrameA, child_focus),
+
+    % wxFocus
+    wxFrame:connect(FrameA, set_focus),
+    wxFrame:connect(FrameA, kill_focus),
+
+    % wxIconize
+    wxFrame:connect(FrameA, iconize),
+
+    % wxKey
     wxFrame:connect(FrameA, 'char'),
     wxFrame:connect(FrameA, key_down),
-    wxFrame:connect(FrameA, move),
-    wxFrame:connect(FrameA, set_focus),
-    wxFrame:connect(FrameA, iconize),
-    wxFrame:connect(FrameA, left_down),
+
+    % wxMouse
     wxFrame:connect(FrameA, motion),
-    wxFrame:connect(FrameA, show),
+    wxFrame:connect(FrameA, left_down),
+    wxFrame:connect(FrameA, left_up),
+    wxFrame:connect(FrameA, middle_down),
+    wxFrame:connect(FrameA, middle_up),
+    wxFrame:connect(FrameA, right_down),
+    wxFrame:connect(FrameA, right_up),
+
+    % wxMove
+    wxFrame:connect(FrameA, move),
+
+    % wxSize
     wxFrame:connect(FrameA, size),
+
+    % wxShow
+    wxFrame:connect(FrameA, show),
+
+    % wxWindowCreate
     wxFrame:connect(FrameA, create),
+
+    % wxWindowDestroy
     wxFrame:connect(FrameA, destroy),
 
     % The text for wxFrame:setStatusText/3 appears at the bottom of the window.
@@ -75,74 +115,155 @@ createWindowA(ServerA) ->
 % Apparently tail-recursive.
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 handleWindowA(FrameA) ->
+    % Note: Should use "if" or "case" statements for the following,
+    % where all events has the same handling for "id" and "obj" etc.
     receive
-        % This is the event which is called when the window is closed.
-        #wx{event=#wxClose{type=close_window}} ->
-            io:format("Process ~p closing window~n",[self()]),
-            ok = wxFrame:setStatusText(FrameA, "Closing soon...", []),
-            timer:sleep(5000),
-            wxWindow:destroy(FrameA),
-            ok;
+        % Try to catch all events in a single case.
+        #wx{id=Id, obj=Obj, event=EvtA} ->
+%            io:format("Process ~p event id=~p, obj=~p, event=~n ~p~n",
+%                [self(), Id, Obj, EvtA]),
 
-        % This event is not received.
-        #wx{event=#wxClose{type=end_session}} ->
-            io:format("Process ~p ending session~n",[self()]),
-            ok = wxFrame:setStatusText(FrameA, "Closing soon...", []),
-            timer:sleep(5000),
-            wxWindow:destroy(FrameA),
-            ok;
+            % Each case must return carry_on or exit_normal.
+            CarryOn = case EvtA of
+            #wxClose{type=TypeA} ->
+                case TypeA of
+                % This is the event which is called when the window is closed.
+                close_window ->
+                    io:format("Process ~p closing window id=~p, obj=~p~n",
+                        [self(), Id, Obj]),
+                    wxFrame:setStatusText(FrameA, "Closing soon...", []),
+                    timer:sleep(2000),
+                    wxWindow:destroy(FrameA),
+%                    exit(normal);
+                    exit_normal;
+                % This event is never received!?
+                end_session ->
+                    io:format("Process ~p ending session id=~p, obj=~p~n",
+                        [self(), Id, Obj]),
+                    wxFrame:setStatusText(FrameA, "Ending session soon...", []),
+                    timer:sleep(2000),
+                    wxWindow:destroy(FrameA),
+%                    exit(normal);
+                    exit_normal;
+                _Else ->
+                    carry_on
+                end;
 
-        % Try to receive a key char event.
-        #wx{event=#wxKey{type=char, keyCode=C}} ->
-            io:format("Process ~p received key char event ~p~n",
-                [self(), C]),
-            handleWindowA(FrameA);
+            #wxIconize{type=TypeA, iconized=Ic} ->
+                case TypeA of
+                iconize ->
+                    io:format("window iconize: iconized=~p~n",
+                        [Ic]);
+                _Else ->
+                    true
+                end,
+                carry_on;
 
-        % Mouse motion.
-        #wx{event=#wxMouse{type=motion, x=X, y=Y}} ->
-            io:format("Process ~p received mouse motion event (~p,~p)~n",
-                [self(), X, Y]),
-            handleWindowA(FrameA);
+            #wxKey{type=TypeA, keyCode=KeyC} ->
+                case TypeA of
+                char ->
+                    io:format("char: keyCode=~p~n",
+                        [KeyC]);
+                key_down ->
+                    io:format("key down: keyCode=~p~n",
+                        [KeyC]);
+                _Else ->
+                    true
+                end,
+                carry_on;
 
-        % Mouse left_down.
-        #wx{event=#wxMouse{type=left_down, x=X, y=Y}} ->
-            io:format("Process ~p received mouse left_down event (~p,~p)~n",
-                [self(), X, Y]),
-            handleWindowA(FrameA);
+            #wxMouse{type=TypeA, x=X, y=Y} ->
+                case TypeA of
+                motion ->
+                    io:format("mouse motion: "
+                        "X=~p, Y=~p~n",
+                        [X, Y]);
+                left_down ->
+                    io:format("mouse left down: "
+                        "X=~p, Y=~p~n",
+                        [X, Y]);
+                left_up ->
+                    io:format("mouse left up: "
+                        "X=~p, Y=~p~n",
+                        [X, Y]);
+                _Else ->
+                    io:format("mouse event: type=~p, "
+                        "X=~p, Y=~p~n",
+                        [TypeA, X, Y])
+                end,
+                carry_on;
 
-        % Window create.
-        #wx{event=#wxWindowCreate{type=create}} ->
-            io:format("Process ~p received window create event~n", [self()]),
-            handleWindowA(FrameA);
+            % The returned rectangle is always {0, 0, 0, 0}. Not good!?
+            #wxMove{type=TypeA, pos={Ws, Hs}, rect={Xr, Yr, Wr, Hr}} ->
+                case TypeA of
+                move ->
+                    io:format("window move: "
+                        "pos=[~p, ~p], rect: X=~p, Y=~p, W=~p, H=~p~n",
+                        [Ws, Hs, Xr, Yr, Wr, Hr]);
+                _Else ->
+                    io:format("window move: unknown type = ~p: "
+                        "pos=[~p, ~p], rect: X=~p, Y=~p, W=~p, H=~p~n",
+                        [TypeA, Ws, Hs, Xr, Yr, Wr, Hr])
+                end,
+                carry_on;
 
-        % Window destroy.
-        #wx{event=#wxWindowDestroy{type=destroy}} ->
-            io:format("Process ~p received window destroy event~n", [self()]),
-            handleWindowA(FrameA);
+            % The returned rectangle is always {0, 0, 0, 0}. Not good!?
+            #wxSize{type=TypeA, size={Ws, Hs}, rect={Xr, Yr, Wr, Hr}} ->
+                case TypeA of
+                size ->
+                    io:format("window size: "
+                        "size=[~p, ~p], rect: X=~p, Y=~p, W=~p, H=~p~n",
+                        [Ws, Hs, Xr, Yr, Wr, Hr]);
+                _Else ->
+                    io:format("window size: unknown type = ~p: "
+                        "size=[~p, ~p], rect: X=~p, Y=~p, W=~p, H=~p~n",
+                        [TypeA, Ws, Hs, Xr, Yr, Wr, Hr])
+                end,
+                carry_on;
 
-        % Window show.
-        #wx{event=#wxShow{type=show}} ->
-            io:format("Process ~p received window show event~n", [self()]),
-            handleWindowA(FrameA);
+            #wxShow{type=TypeA} ->
+                case TypeA of
+                show ->
+                    io:format("window show~n");
+                _Else ->
+                    io:format("window show: unknown type=~p ", [TypeA])
+                end,
+                carry_on;
 
-        % Window size.
-        #wx{event=#wxSize{type=size}} ->
-            io:format("Process ~p received window size event~n", [self()]),
-            handleWindowA(FrameA);
+            #wxWindowCreate{type=TypeA} ->
+                case TypeA of
+                create ->
+                    io:format("window create~n");
+                _Else ->
+                    io:format("window create: unknown type=~p ", [TypeA])
+                end,
+                carry_on;
 
-        % Window move.
-        #wx{event=#wxMove{type=move, pos={X, Y}}} ->
-            io:format("Process ~p received window move event (~p,~p)~n",
-                [self(), X, Y]),
-            handleWindowA(FrameA);
+            #wxWindowDestroy{type=TypeA} ->
+                case TypeA of
+                destroy ->
+                    io:format("window destroy~n");
+                _Else ->
+                    io:format("window destroy: unknown type=~p ", [TypeA])
+                end,
+                carry_on;
 
-        % Window iconize.
-        #wx{event=#wxIconize{type=iconize, iconized=I}} ->
-            io:format("Process ~p received window iconize event ~p~n",
-                [self(), I]),
-            handleWindowA(FrameA);
+            % No matching event class.
+            _Else ->
+                carry_on
+            end,
 
-        % This event is not received.
+            % Use return value of previous "case" to decide whether to carry on.
+            % This will hopefully permit tail recursion, while also
+            % returning to the caller is the inner "case" terminates.
+            if
+                CarryOn == carry_on ->
+                    handleWindowA(FrameA);
+                true ->
+                    ok
+            end;
+
+        % All other event classes which are "connected".
         Evt ->
             io:format("Process ~p received event ~p~n", [self(), Evt]),
             handleWindowA(FrameA)
@@ -176,14 +297,17 @@ startWindowA() ->
     FrameA = wx:batch(fun() -> createWindowA(ServerA) end),
 
     % Show the frame.
+    io:format("Show frame~n", []),
     wxWindow:show(FrameA),
 
     % Go into a loop.
+    io:format("Start event handler~n", []),
     handleWindowA(FrameA),
 
     % Wait for a while.
 %    timer:sleep(5000),
 
     % Destroy everything.
+    io:format("Destroy server~n", []),
     wx:destroy(),
     ok.
