3c5a6639d763e2cd6c518bca8fdcc654e1a71071
Dr. Alan U. Kennington <31448590+drauk@users.noreply.github.com>
2018-02-18 02:11:21 +1100

    Added many more cases for wxErlang event classes.

--- wx1.erl
+++ wx1.erl
@@ -1,4 +1,4 @@
-% src/erlang/wx1.erl   2018-2-17   Alan U. Kennington.
+% src/erlang/wx1.erl   2018-2-18   Alan U. Kennington.
 % $Id$
 % Familiarization with wxErlang library.
 % Reference manual: http://erlang.org/doc/apps/wx/index.html
@@ -48,7 +48,7 @@ createWindowA(ServerA) ->
     % Carriage return. Get the shell text cursor back to the left of the line.
 %    io:format("~n", []),
 
-    % Get keyboard focus.
+    % Set keyboard focus.
     % This seems to have no effect.
 %    wxFrame:setFocus(FrameA),
     wxWindow:setFocus(FrameA),
@@ -65,37 +65,200 @@ createWindowA(ServerA) ->
     wxFrame:connect(FrameA, query_end_session),
 
     % Other event classes and types.
+    % wxActivate
+    wxFrame:connect(FrameA, activate),
+    wxFrame:connect(FrameA, activate_app),
+    wxFrame:connect(FrameA, hibernate),
+
+    % wxAuiManager [Advanced User Interface]
+    % See http://docs.wxwidgets.org/2.8.12/wx_wxauioverview.html#wxauioverview
+    wxFrame:connect(FrameA, aui_pane_button),
+    wxFrame:connect(FrameA, aui_pane_close),
+    wxFrame:connect(FrameA, aui_pane_maximize),
+    wxFrame:connect(FrameA, aui_pane_restore),
+    % The following line gives an error message at run time.
+    % ** exception exit: {{badarg,aui_pane_activated}, ....
+%    wxFrame:connect(FrameA, aui_pane_activated),
+    wxFrame:connect(FrameA, aui_render),
+    wxFrame:connect(FrameA, aui_find_manager),
+
+    % wxAuiNotebook [Advanced User Interface]
+    % (Not interesting enough to list here.)
+
+    % wxCalendar
+    % (Not relevant enough to list here.)
+
     % wxChildFocus
     wxFrame:connect(FrameA, child_focus),
+
+    % wxClipboardText
+    wxFrame:connect(FrameA, command_text_copy),
+    wxFrame:connect(FrameA, command_text_cut),
+    wxFrame:connect(FrameA, command_text_paste),
+
+    % wxColourPicker
+    wxFrame:connect(FrameA, command_colourpicker_changed),
+
+    % wxCommand
+    % (Too big, and not relevant enough to list here.)
+
+    % wxContextMenu
+    wxFrame:connect(FrameA, context_menu),
+
+    % wxDate
+    wxFrame:connect(FrameA, date_changed),
+
+    % wxDisplayChanged
+    wxFrame:connect(FrameA, display_changed),
+
+    % wxDropFiles
+    wxFrame:connect(FrameA, drop_files),
+
+    % wxErase
+    wxFrame:connect(FrameA, erase_background),
+
+    % wxFileDirPiacker
+    % (Not relevant enough to list here.)
 
     % wxFocus
     wxFrame:connect(FrameA, set_focus),
     wxFrame:connect(FrameA, kill_focus),
 
+    % wxFontPicker
+    wxFrame:connect(FrameA, command_fontpicker_changed),
+
+    % wxGrid
+    % (Too many to list.)
+
+    % wxHelp
+    wxFrame:connect(FrameA, help),
+    wxFrame:connect(FrameA, detailed_help),
+
+    % wxHtmlLink
+    wxFrame:connect(FrameA, command_html_link_clicked),
+
     % wxIconize
     wxFrame:connect(FrameA, iconize),
 
+    % wxIdle
+    wxFrame:connect(FrameA, idle),
+
+    % wxInitDialog
+    wxFrame:connect(FrameA, init_dialog),
+
+    % wxJoystick
+    wxFrame:connect(FrameA, joy_button_down),
+    wxFrame:connect(FrameA, joy_button_up),
+    wxFrame:connect(FrameA, joy_move),
+    wxFrame:connect(FrameA, joy_zmove),
+
     % wxKey
     wxFrame:connect(FrameA, 'char'),
+    wxFrame:connect(FrameA, char_hook),
     wxFrame:connect(FrameA, key_down),
+    wxFrame:connect(FrameA, key_up),
+
+    % wxList
+    % (Too many to list.)
+
+    % wxMaximize
+    wxFrame:connect(FrameA, maximize),
+
+    % wxMenu
+    wxFrame:connect(FrameA, menu_open),
+    wxFrame:connect(FrameA, menu_close),
+    wxFrame:connect(FrameA, menu_highlight),
 
     % wxMouse
-    wxFrame:connect(FrameA, motion),
     wxFrame:connect(FrameA, left_down),
     wxFrame:connect(FrameA, left_up),
     wxFrame:connect(FrameA, middle_down),
     wxFrame:connect(FrameA, middle_up),
     wxFrame:connect(FrameA, right_down),
     wxFrame:connect(FrameA, right_up),
+    wxFrame:connect(FrameA, motion),
+    wxFrame:connect(FrameA, enter_window),
+    wxFrame:connect(FrameA, leave_window),
+    wxFrame:connect(FrameA, left_dclick),
+    wxFrame:connect(FrameA, middle_dclick),
+    wxFrame:connect(FrameA, right_dclick),
+    wxFrame:connect(FrameA, mousewheel),
 
     % wxMove
     wxFrame:connect(FrameA, move),
 
-    % wxSize
-    wxFrame:connect(FrameA, size),
+    % wxNavigationKey
+    wxFrame:connect(FrameA, navigation_key),
+
+    % wxNotebook
+    wxFrame:connect(FrameA, command_notebook_page_changed),
+    wxFrame:connect(FrameA, command_notebook_page_changing),
+
+    % wxPaint
+    wxFrame:connect(FrameA, paint),
+
+    % wxPaletteChanged
+    wxFrame:connect(FrameA, palette_changed),
+
+    % wxQueryNewPalette
+    wxFrame:connect(FrameA, query_new_palette),
+
+    % wxSash
+    wxFrame:connect(FrameA, sash_dragged),
+
+    % wxScroll
+    wxFrame:connect(FrameA, scroll_top),
+    wxFrame:connect(FrameA, scroll_bottom),
+    wxFrame:connect(FrameA, scroll_lineup),
+    wxFrame:connect(FrameA, scroll_linedown),
+    wxFrame:connect(FrameA, scroll_pageup),
+    wxFrame:connect(FrameA, scroll_pagedown),
+    wxFrame:connect(FrameA, scroll_thumbtrack),
+    wxFrame:connect(FrameA, scroll_thumbrelease),
+    wxFrame:connect(FrameA, scroll_changed),
+
+    % wxScrollWin
+    wxFrame:connect(FrameA, scrollwin_top),
+    wxFrame:connect(FrameA, scrollwin_bottom),
+    wxFrame:connect(FrameA, scrollwin_lineup),
+    wxFrame:connect(FrameA, scrollwin_linedown),
+    wxFrame:connect(FrameA, scrollwin_pageup),
+    wxFrame:connect(FrameA, scrollwin_pagedown),
+    wxFrame:connect(FrameA, scrollwin_thumbtrack),
+    wxFrame:connect(FrameA, scrollwin_thumbrelease),
+
+    % wxSetCursor
+    wxFrame:connect(FrameA, set_cursor),
 
     % wxShow
     wxFrame:connect(FrameA, show),
+
+    % wxSize
+    wxFrame:connect(FrameA, size),
+
+    % wxSpin
+    wxFrame:connect(FrameA, command_spinctrl_updated),
+    wxFrame:connect(FrameA, spin_up),
+    wxFrame:connect(FrameA, spin_down),
+    wxFrame:connect(FrameA, spin),
+
+    % wxSplitter
+    % (Not relevant enough to list here.)
+
+    % wxStyledText
+    % (Too many to list. Probably not relevant.)
+
+    % wxSysColourChanged
+    wxFrame:connect(FrameA, sys_colour_changed),
+
+    % wxTaskBarIcon
+    % (Too many to list. Probably not relevant.)
+
+    % wxTree
+    % (Too many to list. Probably not relevant.)
+
+    % wxUpdateUI
+    wxFrame:connect(FrameA, update_ui),
 
     % wxWindowCreate
     wxFrame:connect(FrameA, create),
@@ -113,6 +276,14 @@ createWindowA(ServerA) ->
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 % Event-handling loop for FrameA window.
 % Apparently tail-recursive.
+% If exit(normal) is called, this function will not return.
+% So to allow this function to return, the inner case-statement of
+% a nested pair of case-statements returns either carry_on or exit_normal.
+% At the end of the inner case-statement, its output-value is used in
+% an if-statement to determine whether to recurse or just "fall off the end".
+% If the outer case-statement "falls off the end", this function returns to
+% the caller, which is startWindowA/0, which can then call wx:destroy/0.
+% If exit(normal) is called in the inner loop, this function cannot return.
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 handleWindowA(FrameA) ->
     % Note: Should use "if" or "case" statements for the following,
@@ -120,6 +291,7 @@ handleWindowA(FrameA) ->
     receive
         % Try to catch all events in a single case.
         #wx{id=Id, obj=Obj, event=EvtA} ->
+            % Trace all messages.
 %            io:format("Process ~p event id=~p, obj=~p, event=~n ~p~n",
 %                [self(), Id, Obj, EvtA]),
 
@@ -148,6 +320,22 @@ handleWindowA(FrameA) ->
                 _Else ->
                     carry_on
                 end;
+
+            #wxActivate{type=TypeA, active=Ac} ->
+                case TypeA of
+                activate ->
+                    io:format("window activate: active=~p~n",
+                        [Ac]);
+                activate_app ->
+                    io:format("window activate app: active=~p~n",
+                        [Ac]);
+                hibernate ->
+                    io:format("window hibernate: active=~p~n",
+                        [Ac]);
+                _Else ->
+                    true
+                end,
+                carry_on;
 
             #wxIconize{type=TypeA, iconized=Ic} ->
                 case TypeA of
@@ -259,6 +447,8 @@ handleWindowA(FrameA) ->
             if
                 CarryOn == carry_on ->
                     handleWindowA(FrameA);
+                CarryOn == exit_normal ->
+                    ok;
                 true ->
                     ok
             end;
